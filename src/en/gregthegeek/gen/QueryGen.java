4559f8e4fe6afb16ee7b3f40f642d14a64c93319
Greg Carlin <gregthegeek@users.noreply.github.com>
2014-06-13 13:34:58 -0400

    Added more queries and bags

--- src/en/gregthegeek/gen/QueryGen.java
+++ src/en/gregthegeek/gen/QueryGen.java
@@ -3,15 +3,18 @@ package en.gregthegeek.gen;
 import 
 
 public class QueryGen {
-    private static final String TRUE = "\"true\"";
+    private static final String TRUE  = "\"true\"";
     private static final String FALSE = "\"false\"";
     
+    private static final Object[][] NULL        = new Object[   0][0];
     private static final Object[][] NUM_PAIRS   = new Object[2048][2];
     private static final Object[][] NUM_SINGS   = new Object[2048][1];
     private static final Object[][] LOGIC_PAIRS = new Object[   4][2];
     private static final Object[][] LOGIC_SINGS = new Object[   2][1];
     private static final Object[][] STR_PAIRS   = new Object[2048][2];
     private static final Object[][] STR_SINGS   = new Object[2048][1];
+    private static final Object[][] DATE_OPTS   = new Object[   4][1];
+    private static final Object[][] BAGS        = new Object[2048][1];
     
     static {
         for(int i=0; i<NUM_PAIRS.length; i++) {
@@ -38,18 +41,23 @@ public class QueryGen {
         LOGIC_SINGS[0][0] = TRUE;
         LOGIC_SINGS[1][0] = FALSE;
         
+        StringBuilder sb = new StringBuilder();
         for(int i=0; i<STR_PAIRS.length; i++) {
             STR_PAIRS[i][0] = "asdf";
-            STR_PAIRS[i][1] = getString(i + 1); // TODO cache StringBuilder to improve speed
+            sb.append((char) (((i + 1) % 26) + 97));
+            STR_PAIRS[i][1] = sb.toString();
         }
-    }
-    
-    private static String getString(int len) {
-        StringBuilder sb = new StringBuilder();
-        for(int i=0; i<len; i++) {
-            sb.append((char) ((i % 26) + 97));
+        
+        DATE_OPTS[0][0] = "low";
+        DATE_OPTS[1][0] = "medium";
+        DATE_OPTS[2][0] = "high";
+        DATE_OPTS[3][0] = "full";
+        
+        StringBuilder sbBags = new StringBuilder();
+        for(int i=0; i<BAGS.length; i++) {
+            sbBags.append(i).append(", ");
+            BAGS[i] = sbBags.substring(0, sbBags.length() - 2);
         }
-        return sb.toString();
     }
     
     public static void main(String[] args) {
@@ -66,7 +74,31 @@ public class QueryGen {
         Query or  = new Query("%s or %s" , LOGIC_PAIRS);
         Query not = new Query("not %s"   , LOGIC_SINGS);
         
-        Query cat = new Query("%s + %s", STR_PAIRS);
+        Query cat = new Query("%s + %s"  , STR_PAIRS);
+        
+        Query eqNum  = new Query("%d = %d" , NUM_PAIRS);
+        Query eqStr  = new Query("%s = %s" , STR_PAIRS);
+        Query neqNum = new Query("%d <> %d", NUM_PAIRS);
+        Query neqStr = new Query("%s <> %s", STR_PAIRS);
+        
+        Query gr  = new Query("%d > %d" , NUM_PAIRS);
+        Query ls  = new Query("%d < %d" , NUM_PAIRS);
+        Query gre = new Query("%d >= %d", NUM_PAIRS);
+        Query lse = new Query("%d <= %d", NUM_PAIRS);
+        
+        // TODO <str> ~~ <regex>
+        // TODO <str> !~ <regex>
+        // TODO <x> as <y>
+        // TODO <x> groupas <y>
+        
+        Query now   = new Query("now()");
+        Query dform = new Query("dateprec(1900-01-01, %s)", DATE_OPTS);
+        
+        Query sum = new Query("sum %s"  , BAGS);
+        Query cnt = new Query("count %s", BAGS);
+        Query min = new Query("min %s"  , BAGS);
+        Query max = new Query("max %s"  , BAGS);
+        Query avg = new Query("avg %s"  , BAGS);
         
         bw.close();
     }
@@ -74,6 +106,10 @@ public class QueryGen {
     private static class Query() {
         private final String string;
         private final Object[][] args;
+        
+        public Query(String string) {
+            this(string, NULL);
+        }
         
         public Query(String string, Object[][] args) {
             this.string = string + ";";
